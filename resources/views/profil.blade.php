a71467166b1c59fb46d8ae26b2db88dba1ed73ab
Franzz-jeremy <nasutionabdul247@gmail.com>
2025-09-25 14:29:53 +0700

    penambahan halaman baru

--- resources/views/profil.blade.php
+++ resources/views/profil.blade.php
@@ -1,11 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ini profile</title>
-</head>
-<body>
-    ini halaman profile 
-</body>
-</html>
+<h1>INI HALAMAN PROFIL</h1>
+
+<ul>
+    <li><a href="/home">Home</a></li>
+    <li><a href="/about">About</a></li>
+    <li><a href="/profil">Profil</a></li>
+</ul>
